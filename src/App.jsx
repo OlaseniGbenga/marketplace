8a745fc95296dded057d529c31950f0e49af7fd7
Papi <gj.olaseni@gmail.com>
2023-08-01 11:10:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "./pages/home";
+
+function App() {
+  
+
+  return (
+    
+      <div className='bg-red-600'>
+        <Home/>
+      </div>
+      
+    
+  )
+}
+
+export default App
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div class="flex justify-center items-center h-screen">
+    <h1 class="text-4xl font-bold transform transition-transform hover:rotate-y-180">
+      Hover me
+    </h1>
+  </div>
+  
+
+    
+  )
+}
+
+export default Home
