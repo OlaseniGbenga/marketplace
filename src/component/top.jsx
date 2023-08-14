c712a72559b08d375cec020412387506676a1ef5
Papi <gj.olaseni@gmail.com>
2023-08-14 08:47:42 +0100

    done with nav bar and it fully responsive

--- src/component/top.jsx
+++ src/component/top.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 // font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -17,9 +17,19 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 function Top() {
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isMavVisible, setMavVisibility] = useState(true);
+  
+
+  const toggleNavVisibility = () => {
+    setNavVisibility(!isNavVisible);
+    setMavVisibility(!isMavVisible);
+  };
+
   return (
     <header className=" font-Gilroy ">
       <div className="">
+        {/* nav for lg screen */}
         <div className="md:block hidden">
           <ul className=" text-Black w-full bg-White border  border-solid border-Black sm:grid sm:grid-cols-8   flex flex-col   ">
             <li className="col-start-1 border sm:border-y-0 sm:border-r-0 border-b-0 border-solid border-Black flex items-center sm:justify-center  transform transition-transform hover:rotate-y-180 py-[2rem] px-[1.5rem]">
@@ -36,36 +46,44 @@ function Top() {
             </li>
           </ul>
         </div>
-        <div className="md:hidden  flex justify-between border solid border-Black">
-          <span className="p-4 border border-y-0 border-l-0 border-x-1 border-Black">
+
+        {/* nav for small screen */}
+       { isMavVisible && (<div className="md:hidden  flex justify-between border solid border-Black">
+          <span onClick={toggleNavVisibility} className="p-4 border border-y-0 border-l-0 border-x-1 border-Black">
             <FontAwesomeIcon icon={faBars} />
           </span>
           <span className="p-4 border border-y-0 border-r-0 border-x-1 border-Black ">
             <FontAwesomeIcon icon={faBagShopping} />
           </span>
-        </div>{" "}
-        {/* <div className=" md:hidden sm:w-1/2"> 
-          <span className="p-6  border border-Black block "><FontAwesomeIcon icon={faXmark} /></span>
-          <p className="p-6  border border-Black border-t-0">sign in</p>
-          <p className="p-6  border border-Black border-t-0">Shop</p>
-          <p className="p-6  border border-Black border-t-0">Service</p>
-          <p className="p-6  border border-Black border-t-0">Contact</p>
-          <p className="p-6  border border-Black border-t-0">About Us</p>
-          <div className="p-6  border border-Black border-t-0">
-            <p>Shipping & returns</p>
-            <p>Terms & Conditions</p>
-            <p>Private policy</p>
-          </div>
-          <div className="p-6  border border-Black border-t-0">
-          <FontAwesomeIcon icon={faInstagram} />
-          <FontAwesomeIcon icon={faPinterest} />
-          <FontAwesomeIcon icon={faFacebookF} />
-          <FontAwesomeIcon icon={faXTwitter} />
-          <FontAwesomeIcon icon={faTelegram} />
-          <FontAwesomeIcon icon={faBars} />
-        </div>
+        </div>)}
 
-        </div> */}
+        {/* toggled menue */}
+
+        {isNavVisible && (
+          <div className=" md:hidden sm:w-1/2">
+            <span onClick={toggleNavVisibility} className="p-6  border border-Black block ">
+              <FontAwesomeIcon icon={faXmark} />
+            </span>
+            <p className="p-6  border border-Black border-t-0">sign in</p>
+            <p className="p-6  border border-Black border-t-0">Shop</p>
+            <p className="p-6  border border-Black border-t-0">Service</p>
+            <p className="p-6  border border-Black border-t-0">Contact</p>
+            <p className="p-6  border border-Black border-t-0">About Us</p>
+            <div className="p-6  border border-Black border-t-0">
+              <p>Shipping & returns</p>
+              <p>Terms & Conditions</p>
+              <p>Private policy</p>
+            </div>
+            <div className="p-6  flex justify-between border border-Black border-t-0">
+              <FontAwesomeIcon icon={faInstagram} />
+              <FontAwesomeIcon icon={faPinterest} />
+              <FontAwesomeIcon icon={faFacebookF} />
+              <FontAwesomeIcon icon={faXTwitter} />
+              <FontAwesomeIcon icon={faTelegram} />
+              
+            </div>
+          </div>
+        )}
       </div>
     </header>
   );
